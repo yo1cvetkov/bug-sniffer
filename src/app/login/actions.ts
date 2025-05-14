e26f8a22de4c4f4d48d48cf31ae4f99062540a2d
Jovan Cvetkov <yo1.cvetkov@gmail.com>
2025-05-14 22:49:17 +0200

    login action redirection fixed

--- src/app/login/actions.ts
+++ src/app/login/actions.ts
@@ -19,7 +19,7 @@ export async function login(formData: FormData) {
   }
 
   revalidatePath('/', 'layout');
-  redirect('/account');
+  redirect('/');
 }
 
 export async function signup(formData: FormData) {
